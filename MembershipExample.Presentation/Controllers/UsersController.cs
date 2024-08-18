e35826d4bcafce761da7f28c38ebe5105199416b
Martin Serdenia <martinserdenia@gmail.com>
2024-08-18 22:28:01 +0800

    added more api routes

--- MembershipExample.Presentation/Controllers/UsersController.cs
+++ MembershipExample.Presentation/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using MembershipExample.Application.DTOs;
+using MembershipExample.Application.Exceptions;
 using MembershipExample.Application.Features.Users.Commands;
 using MembershipExample.Application.Features.Users.Queries;
 using Microsoft.AspNetCore.Mvc;
@@ -20,39 +22,88 @@ namespace MembershipExample.Api.Controllers
         [HttpGet]
         public async Task<ActionResult<List<UserDto>>> GetUsers()
         {
-            var query = new GetUsersQuery();
-            var result = await _mediator.Send(query);
-            return Ok(result);
+            try
+            {
+                var query = new GetUsersQuery();
+                var result = await _mediator.Send(query);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<UserDto>> GetUser(int id)
         {
-            var query = new GetUserByIdQuery(id);
-            var result = await _mediator.Send(query);
-            return result != null ? Ok(result) : NotFound();
+            try
+            {
+                var query = new GetUserByIdQuery(id);
+                var result = await _mediator.Send(query);
+                return result != null ? Ok(result) : NotFound();
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
         }
 
         [HttpPost]
         public async Task<ActionResult<UserDto>> CreateUser(CreateUserCommand command)
         {
-            var result = await _mediator.Send(command);
-            return CreatedAtAction(nameof(GetUser), new { id = result.Id }, result);
+            try
+            {
+                var result = await _mediator.Send(command);
+                return CreatedAtAction(nameof(GetUser), new { id = result.Id }, result);
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
         }
 
         [HttpPut]
-        public async Task<ActionResult<UserDto>> UpdateUser(UpdateUserCommand command)
+        public async Task<ActionResult<UserDto>> UpdateUser(int id, UpdateUserCommand command)
         {
-            var result = await _mediator.Send(command);
-            return CreatedAtAction(nameof(GetUser), new { id = result.Id }, result);
+            try
+            {
+                if(id != command.Id)
+                    throw new ValidationException("Id in the body does not match the Id in the URL");
+
+                var result = await _mediator.Send(command);
+                return CreatedAtAction(nameof(GetUser), new { id = result.Id }, result);
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteUser(int id)
         {
-            var command = new DeleteUserCommand(id);
-            await _mediator.Send(command);
-            return NoContent();
+            try
+            {
+                var command = new DeleteUserCommand(id);
+                await _mediator.Send(command);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
+
+        private ActionResult HandleException(Exception ex)
+        {
+            return ex switch
+            {
+                UserNotFoundException => NotFound(new { error = ex.Message }),
+                UsernameAlreadyTakenException => BadRequest(new { error = ex.Message }),
+                ValidationException => BadRequest(new { error = ex.Message }),
+                _ => StatusCode(500, new { error = ex.Message })
+            };
         }
     }
 
